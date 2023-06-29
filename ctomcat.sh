16454bd0d37bd5d9967ddca7be28966b3e0cf404
nag9853 <135331789+nag9853@users.noreply.github.com>
2023-06-29 12:00:51 +0530

    Update ctomcat.sh

--- ctomcat.sh
+++ ctomcat.sh
@@ -6,13 +6,13 @@ sudo apt-get install openjdk-11-jre-headless -y
 sudo apt-get install zip -y
 sudo wget https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.76/bin/apache-tomcat-9.0.76.tar.gz
 sudo tar -xvzf apache-tomcat-9.0.76.tar.gz -C /opt
-sudo mv apache-tomcat-9.0.76/ apache-tomcat9
+sudo mv /opt/apache-tomcat-9.0.76/ /opt/apache-tomcat9
 sudo useradd -M -d /opt/apache-tomcat9/ tomcat
-sudo chown -R tomcat. /opt/apache-tomcat9/
-sudo sed -i '21,22 s/^/<!-- /; 21,22 s/$/ -->/' /opt/tomcat9/webapps/manager/META-INF/context.xml
-sudo sed -i '21,22 s/^/<!-- /; 21,22 s/$/ -->/' /opt/tomcat9/webapps/host-manager/META-INF/context.xml
+sudo chown -R tomcat.tomcat /opt/apache-tomcat9
+sudo sed -i '21,22 s/^/<!-- /; 21,22 s/$/ -->/' /opt/apache-tomcat9/webapps/manager/META-INF/context.xml
+sudo sed -i '21,22 s/^/<!-- /; 21,22 s/$/ -->/' /opt/apache-tomcat9/webapps/host-manager/META-INF/context.xml
 sed -i '/<\/tomcat-users>/i \
 <role rolename="manager-gui"/>\
 <role rolename="admin-gui"/>\
 <user username="manager" password="12345" roles="manager-gui"/>\
-<user username="admin" password="12345" roles="admin-gui"/>' /opt/tomcat9/conf/tomcat-users.xml
+<user username="admin" password="12345" roles="admin-gui"/>' /opt/apache-tomcat9/conf/tomcat-users.xml
